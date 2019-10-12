7c52c3942914824c12b7a6551f2ae8533526fe42
Fhzuccoli <fhzuccoli@gmail.com>
2019-10-12 18:57:35 -0300

    Testes iniciais com movimentos possiveis das pecas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -10,25 +10,40 @@ namespace xadrez_console
         static void Main(string[] args)
         {
 
-            PartidaDeXadrez partida = new PartidaDeXadrez();
-
-            while (!partida.terminada)
+            try
             {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
+
                 Console.Clear();
                 Tela.imprimirTabuleiro(partida.tab);
-
-                Console.WriteLine();
-                Console.Write("Origem: ");
-                Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
-                Console.Write("Destino: ");
-                Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
-
-                partida.executaMovimento(origem, destino);
             }
 
-            Tela.imprimirTabuleiro(partida.tab);
-
-            Console.WriteLine();
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine();
+            }
         }
     }
 }
